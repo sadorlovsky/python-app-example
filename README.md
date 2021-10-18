ac00fe8aff89fb1d746a33d850ab750155131bd9
Zach Orlovsky <sadorlovsky@gmail.com>
2021-10-19 01:44:25 +0300

    poetry 📖

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Python HTTP app example
+# Python app example
 
 ## Code
 
--- app/main.py
+++ app/main.py
@@ -4,14 +4,16 @@ Python app example
 from sanic import Sanic
 from sanic.response import json
 
-app = Sanic(name='app-example')
+app = Sanic(name="app-example")
+
 
 @app.route("/")
-async def index():
+async def index(_request):
     """
     Returns sample JSON data
     """
-    return json({ "data": "Hello World" })
+    return json({"data": "Hello World"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
