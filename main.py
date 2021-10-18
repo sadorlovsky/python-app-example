fd39e9cd197892582856d005168d5dad021efcf6
Zach Orlovsky <sadorlovsky@gmail.com>
2021-10-19 01:14:52 +0300

    add pylint

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+"""
+Python app example
+"""
 from sanic import Sanic
 from sanic.response import json
 
 app = Sanic(name='app-example')
 
 @app.route("/")
-async def index(request):
-	return json({ "data": "Hello World" })
+async def index():
+    """
+    Returns sample JSON data
+    """
+    return json({ "data": "Hello World" })
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000)
